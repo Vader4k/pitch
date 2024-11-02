84c50def0a9773e4eac9e47b7271846deb433e16
Vader4k <bigkingkush295@gmail.com>
2024-11-02 11:34:21 +0100

    @add/ sucessfully gotten playlist info on frontend

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,9 +1,6 @@
 import { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
-import {
-  STARTUP_BY_ID_QUERY,
-  PLAYLIST_BY_SLUG
-} from "@/sanity/lib/queries";
+import { STARTUP_BY_ID_QUERY, PLAYLIST_BY_SLUG } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import markdownit from "markdown-it";
 import { formatDate } from "@/lib/utils";
@@ -11,7 +8,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
-import { groq } from "next-sanity";
+import { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard from "@/components/StartupCard";
 
 const md = markdownit();
 export const experimental_ppr = true;
@@ -21,8 +19,11 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const details = await client.fetch(STARTUP_BY_ID_QUERY, { id });
   if (!details) return notFound();
 
-  const posts = await client.fetch(PLAYLIST_BY_SLUG, {slug : 'editors-pick'})
-  console.log('this is playlist', posts)
+  const editorsPick = await client.fetch(PLAYLIST_BY_SLUG, {
+    slug: "editors-pick",
+  });
+
+
 
   //store the mackdown content from the posts in a parsedcontent variable
   const parsedContent = md.render(details?.pitch || "");
@@ -75,6 +76,19 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
           )}
         </div>
         <hr className="divider" />
+
+        {editorsPick?.select?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorsPick?.select.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
         {/* todo selection startup */}
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
